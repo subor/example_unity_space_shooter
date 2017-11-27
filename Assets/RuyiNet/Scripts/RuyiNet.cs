274b7f841973eddde5a786e1678acfeaeb5ab390
PLAYRUYI\mark <mark.mcdonagh@playruyi.com>
2017-11-27 17:49:13 +0800

    [JP-1155] Latest Demo files.

--- Assets/RuyiNet/Scripts/RuyiNet.cs
+++ Assets/RuyiNet/Scripts/RuyiNet.cs
@@ -35,6 +35,8 @@ public class RuyiNet : MonoBehaviour
         };
 
         mSDK = RuyiSDK.CreateInstance(mSDKContext);
+
+        //DontDestroyOnLoad(this);
     }
 
     private void Update()
--- Assets/_Completed-Assets/Scripts/Done_PlayerController.cs
+++ Assets/_Completed-Assets/Scripts/Done_PlayerController.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.Networking;
 
 [System.Serializable]
 public class Done_Boundary 
@@ -7,7 +7,7 @@ public class Done_Boundary
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class Done_PlayerController : MonoBehaviour
+public class Done_PlayerController : NetworkBehaviour
 {
 	public float speed;
 	public float tilt;
@@ -23,31 +23,37 @@ public class Done_PlayerController : MonoBehaviour
 	
 	void Update ()
 	{
-		if (Input.GetButton("Fire" + PlayerIndex) && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			var bullet = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-            var mover = bullet.GetComponent<Done_Mover>();
-            mover.Index = PlayerIndex - 1;
-            GetComponent<AudioSource>().Play ();
-		}
+        if (isLocalPlayer)
+        {
+            if (Input.GetButton("Fire" + PlayerIndex) && Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                var bullet = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+                var mover = bullet.GetComponent<Done_Mover>();
+                mover.Index = PlayerIndex - 1;
+                GetComponent<AudioSource>().Play();
+            }
+        }
 	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal" + PlayerIndex);
-		float moveVertical = Input.GetAxis ("Vertical" + PlayerIndex);
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal" + PlayerIndex);
+            float moveVertical = Input.GetAxis("Vertical" + PlayerIndex);
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		GetComponent<Rigidbody>().velocity = movement * speed;
-		
-		GetComponent<Rigidbody>().position = new Vector3
-		(
-			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
-			0.0f, 
-			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-		);
-		
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            GetComponent<Rigidbody>().velocity = movement * speed;
+
+            GetComponent<Rigidbody>().position = new Vector3
+            (
+                Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+
+            GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+        }
 	}
 }
