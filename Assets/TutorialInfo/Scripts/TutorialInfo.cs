0beefc845d5244d2a1817349e339727f06079516
PLAYRUYI\Goden <goden.zheng@playruyi.com>
2018-06-06 17:48:29 +0800

    [jp_2150] remove unnecessary comment

--- Assets/TutorialInfo/Scripts/TutorialInfo.cs
+++ Assets/TutorialInfo/Scripts/TutorialInfo.cs
@@ -35,29 +35,16 @@ public class TutorialInfo : MonoBehaviour
 
         //our input event is listener in sub-thread, in which you can't directly renderer UnityEngine Object (you can't use any UnityEngine-related object in sub-thread)
         //you can use middle values £¨int,float,string,etc,non-UnityEngine-ojbect-type£© to receive the RuyiSDK input value, then listen it in UnityEngine's main thread (Monobehaviour.update(), etc)
-        //ruyiNet.Subscribe.Subscribe("service/" + Ruyi.Layer0.ServiceIDs.USER_SERVICE_EXTERNAL.ToString().ToLower());
-        //ruyiNet.Subscribe.AddMessageHandler<Ruyi.SDK.UserServiceExternal.InputActionEvent>(RuyiInputStateChangeHandler);
-        //ruyiNet.Subscribe.Subscribe("service/" + Ruyi.Layer0.ServiceIDs.INPUTMANAGER_INTERNAL.ToString().ToLower());
         ruyiNet.Subscribe.Subscribe("service/inputmgr_int");
         ruyiNet.Subscribe.AddMessageHandler<Ruyi.SDK.InputManager.RuyiGamePadInput>(RuyiGamePadInputListener);
         ruyiNet.Subscribe.AddMessageHandler<Ruyi.SDK.InputManager.RuyiKeyboardInput>(RuyiKeyboardInputListener);
         ruyiNet.Subscribe.AddMessageHandler<Ruyi.SDK.InputManager.RuyiMouseInput>(RuyiMouseInputListener);
-
-        //Debug.Log("TutorialInfo Start !!!");
     }
 
     void RuyiGamePadInputListener(string topic, Ruyi.SDK.InputManager.RuyiGamePadInput msg)
     {       
         float leftThumbX = MappingThumbValue(msg.LeftThumbX);
-        float leftThumbY = MappingThumbValue(msg.LeftThumbY);
-
-        //leftThumbX(Y) value ranges from -1 to 1 which represents joystick left to right
-        //generally abs(leftThumbx(Y)) samller than 0.1 means release the joystick
-
-        //Debug.Log("RuyiGamePadInputListener RuyiGamePadInput LeftThumbX:" + leftThumbX);
-        //Debug.Log("RuyiGamePadInputListener RuyiGamePadInput LeftThumbX:" + leftThumbY);
-
-        Debug.Log("RuyiGamePadInputListener RuyiGamePadInput ButtonFlags:" + msg.ButtonFlags);
+        float leftThumbY = MappingThumbValue(msg.LeftThumbY);   
 
         // add condition ture == m_IsLJoystickDown to avoid repeated operation 
         // because joystick event will call continuesly while pushing the joystick
@@ -103,12 +90,12 @@ public class TutorialInfo : MonoBehaviour
 
     void RuyiKeyboardInputListener(string topic, Ruyi.SDK.InputManager.RuyiKeyboardInput msg)
     {
-        //Debug.Log("TutorialInfo RuyiKeyboardInputListener topic:" + topic);
+
     }
 
     void RuyiMouseInputListener(string topic, Ruyi.SDK.InputManager.RuyiMouseInput msg)
     {
-        //Debug.Log("TutorialInfo RuyiMouseInputListener topic:" + topic);
+
     }
 
     private float MappingThumbValue(float value)
@@ -123,8 +110,7 @@ public class TutorialInfo : MonoBehaviour
     }  
 
     private void RuyiInputListener()
-    {
-        
+    {        
         //Debug.Log("RuyiInputListener m_IsBtnSelectedChanged:" + m_IsBtnSelectedChanged + " m_IsEnter:" + m_IsEnter);
         if (m_IsBtnSelectedChanged)
         {
@@ -154,84 +140,6 @@ public class TutorialInfo : MonoBehaviour
         if (m_IsReturn)
         {
             m_IsReturn = false;           
-        }
-    }
-
-    void RuyiInputStateChangeHandler(string topic, Ruyi.SDK.UserServiceExternal.InputActionEvent msg)
-    {
-        //TriggerKeys 
-        //DeviceType: to identify your input device
-        //Key: the key of your input device
-        //action: use this value to identify the input button, this value can be configed in the config file
-        //NewValue/OldValue:  could be three value:0,1,2.  1 means press Down 2 means release 0 not define yet
-        //NewValue is the current key state, if your press down, NewValue will be 1, when you release, NewValue will be 2, OldValue will be 1
-
-        //you can judge the input key by "action" value of Triggers structure. The value of "action" can be modified
-        //in config file of the game package. Now I just hard-core in code. We'll try to optimise this part
-        //in future release
-        //all default system action value: (Layer0/RuyiLocalRoot/Resources/configs/UserSetting)
-        //GamePad_LB
-        //GamePad_LT
-        //GamePad_L3
-        //GamePad_RB
-        //GamePad_RT
-        //GamePad_R3
-        //GamePad_UP
-        //GamePad_Down
-        //GamePad_Left
-        //GamePad_Down
-        //GamePad_Home
-        //GamePad_Back
-        //GamePad_Start
-        //GamePad_X
-        //GamePad_Y
-        //GamePad_A
-        //GamePad_B
-        //GamePad_LJoyX
-        //GamePad_LJoyY
-        //GamePad_RJoyX
-        //GamePad_RJoyY
-        for (int i = 0; i < msg.Triggers.Count; ++i)
-        {           
-            //Debug.Log("TutorialInfo RuyiInputStateChangeHandler topic:" + topic + " action:" + msg.Action + " key:" + msg.Triggers[i].Key + " newValue:" + msg.Triggers[i].NewValue);
-            if (msg.Action.Equals("GamePad_LJoyX"))
-            {
-                byte axisValue = (byte)msg.Triggers[i].NewValue;
-                //Debug.Log("TutorialInfo GamePad_LJoyX value:" + axisValue);
-            }
-            if (msg.Action.Equals("GamePad_RJoyX"))
-            {
-                byte axisValue = (byte)msg.Triggers[i].NewValue;
-                //Debug.Log("TutorialInfo GamePad_RJoyX value:" + axisValue);
-            }
-            if (msg.Action.Equals("GamePad_LJoyY"))
-            {
-                byte axisValue = (byte)msg.Triggers[i].NewValue;
-                Debug.Log("TutorialInfo GamePad_LJoyY value:" + axisValue);
-            }
-            if (msg.Action.Equals("GamePad_RJoyY"))
-            {
-                byte axisValue = (byte)msg.Triggers[i].NewValue;
-                //Debug.Log("TutorialInfo GamePad_RJoyY value:" + axisValue);
-            }
-            if (msg.Action.Equals("GamePad_Up") && 1 == msg.Triggers[i].NewValue)
-            {
-                --m_BtnSelected;
-                m_IsBtnSelectedChanged = true;
-            }
-            if (msg.Action.Equals("GamePad_Down") && 1 == msg.Triggers[i].NewValue)
-            {
-                ++m_BtnSelected;
-                m_IsBtnSelectedChanged = true;
-            }
-            if (msg.Action.Equals("GamePad_A") && 1 == msg.Triggers[i].NewValue)
-            {
-                m_IsEnter = true;
-            }
-            if (msg.Action.Equals("GamePad_B") && 1 == msg.Triggers[i].NewValue)
-            {
-                m_IsReturn = true;
-            }          
         }
     }
 
